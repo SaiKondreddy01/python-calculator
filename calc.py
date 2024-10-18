c22dce7a66656e378af37b14971c6514c4769311
SaiKondreddy01 <sai.kondreddy01@gmail.com>
2024-10-17 18:22:53 -0700

    add subtraction

--- calc.py
+++ calc.py
@@ -1,4 +1,11 @@
-a = input("what is your first number? ")
-b = input("what is your second number? ")
-c = int(a) + int(b)
-print(str (c))
+thing = input("Do you want addition, subtraction, multiplication, or division? ")
+if thing == addition:
+  a = input("what is your first number? ")
+  b = input("what is your second number? ")
+  c = int(a) + int(b)
+  print(str (c))
+if thing == subtraction:
+  a = input("what is your first number? ")
+  b = input("what is your second number? ")
+  c = int(a) - int(b)
+  print(str (c))
